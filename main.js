8583d677f7b4df44bce918f2c0bceaffa0bf18c3
fofoiraq <fofoiraq23@gmail.com>
2018-10-15 16:35:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ var exports = module.exports = {};
 const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("../config.json");
 const botDataJson = require("./../botData.json");
 const botCommands = require("./bot-commands.json");
 // Delete this line when you´re using this project for public usages.
-const prv_config = require("../private_config.json");
+
 const now = require("performance-now");
 
 
